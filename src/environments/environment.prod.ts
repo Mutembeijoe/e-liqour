4af824970a02b0cb409239513f9ccf1f8a30a6b2
mutembeijoe <mutembeijoe@gmail.com>
2019-12-03 15:34:54 +0300

    adds firebase configuration to environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDWELSkH9icd-rVBAgn-KwWqSPh_pPRBz8",
+    authDomain: "my-shop-6cd77.firebaseapp.com",
+    databaseURL: "https://my-shop-6cd77.firebaseio.com",
+    projectId: "my-shop-6cd77",
+    storageBucket: "my-shop-6cd77.appspot.com",
+    messagingSenderId: "155934148041",
+    appId: "1:155934148041:web:2d33a462b08d969e2ed418"
+  }
 };
