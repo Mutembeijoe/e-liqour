020f775d01ce2e2d1f1feeee6377c3b28659b4c1
mutembeijoe <mutembeijoe@gmail.com>
2019-12-05 16:22:08 +0300

    added  a data table to the admin product management page, also imports prebuilt themes for angular materials

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,6 +1,8 @@
 import { ProductsService } from 'src/app/services/products.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Product } from 'src/app/models/product';
+import { DataTableResource } from 'angular7-data-table';
 
 @Component({
   selector: 'app-admin-products',
@@ -8,13 +10,30 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
-  products: any[];
+  products: Product[];
   filteredProducts: any[];
   subscription: Subscription;
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
+
   constructor(private productsS: ProductsService) {
-    this.subscription = productsS.getAll().subscribe(products => this.filteredProducts = this.products = products);
+    this.subscription = this.productsS.getAll().subscribe((products: Product[]) => {
+      this.filteredProducts = this.products = products;
+      this.initializeTable(products);
+    })
   }
 
+  private initializeTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0 }).then(items => this.items = items);
+    this.tableResource.count().then(count => this.itemCount = count);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) { return; }
+    this.tableResource.query(params).then(items => this.items = items);
+  }
   ngOnInit() {
   }
 
