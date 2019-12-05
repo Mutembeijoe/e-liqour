2cbc3b770f3c3e2a938d9048370c6f39c5668bf2
mutembeijoe <mutembeijoe@gmail.com>
2019-12-05 14:50:23 +0300

    adds search functionality to filter products on an the admin page

--- src/app/components/admin/admin-products/admin-products.component.ts
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -1,18 +1,34 @@
 import { ProductsService } from 'src/app/services/products.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: any[];
+  filteredProducts: any[];
+  subscription: Subscription;
   constructor(private productsS: ProductsService) {
-    this.products$ = productsS.getAll();
+    this.subscription = productsS.getAll().subscribe(products => this.filteredProducts = this.products = products);
   }
 
   ngOnInit() {
   }
+
+  filter(query) {
+
+    this.filteredProducts = (query) ? this.products.filter(product => product.title.toLowerCase().includes(query.toLowerCase()))
+      : this.filteredProducts = this.products;
+
+
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
 
 }
