a11ce7332ab0e0651535c72b0a25967d1044730f
mutembeijoe <mutembeijoe@gmail.com>
2019-12-06 11:37:40 +0300

    adds a category tabs nav with logic to filter products based on category

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,9 @@
+import { Product } from './../../models/product';
 import { ProductsService } from './../../services/products.service';
 import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products$;
-  constructor(private productS: ProductsService) {
-    this.products$ = this.productS.getAll();
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories$;
+  category: string;
+  constructor(private productS: ProductsService, private categoryS: CategoriesService, private route: ActivatedRoute) {
+    this.productS.getAll().pipe(
+      switchMap((products: Product[]) => {
+        this.products = products;
+        return this.route.queryParamMap;
+      })
+    ).subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ? this.products.filter(product => product.category === this.category) : this.products;
+    });
+
+    this.categories$ = this.categoryS.getAll();
+
   }
 
   ngOnInit() {
