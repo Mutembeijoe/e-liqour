f4430e85b38fd67519eb608294da233414f99856
mutembeijoe <mutembeijoe@gmail.com>
2019-12-06 08:53:24 +0300

    refactors code, changes how product categories are saved to the db, new code saves the category key rather than category name

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,10 @@ export class CategoriesService {
   constructor(private db: AngularFireDatabase) { }
 
   getCategories() {
-    return this.db.list('/categories').valueChanges();
+    return this.db.list('/categories').snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
   }
 }
