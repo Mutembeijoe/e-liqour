583b0de1e33c1b4587d2b6242dd743f77aa9fbd6
mutembeijoe <mutembeijoe@gmail.com>
2019-12-08 11:34:00 +0300

    add a shopping cart service

---
+++ src/app/services/shopping-cart.service.ts
@@ -0,0 +1,54 @@
+import { CartItem } from './../models/cart-item';
+import { Product } from './../models/product';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
+import { Injectable } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  private create() {
+    return this.db.list('/shopping-carts/').push({
+      dateCreated: new Date().getTime()
+    });
+  }
+
+  async getCart() {
+    const cartId = await this.getOrCreateCart();
+    return this.db.object(`/shopping-carts/${cartId}`).valueChanges();
+  }
+
+  private getItem(cartId, productId) {
+    return this.db.object(`/shopping-carts/${cartId}/items/${productId}`);
+  }
+
+  private async getOrCreateCart() {
+    const cartId = localStorage.getItem('cartId');
+
+    if (cartId) {
+      return cartId;
+    }
+
+    const result = this.create();
+    localStorage.setItem('cartId', result.key);
+    return result.key;
+  }
+
+  async addToCart(product: Product) {
+    const cartId = await this.getOrCreateCart();
+    const item$ = this.getItem(cartId, product.key);
+    item$.snapshotChanges().pipe(take(1)).subscribe(item => {
+      if (item.payload.exists()) {
+        const myItem = item.payload.val();
+        item$.update({ product, quantity: (myItem['quantity'] || 0) + 1 });
+      } else {
+        item$.set({ product, quantity: 1 });
+      }
+    });
+  }
+}
