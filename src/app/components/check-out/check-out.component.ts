ad791fe48cf2c922ad28923d70a8cbcd657350b0
mutembeijoe <mutembeijoe@gmail.com>
2019-12-10 08:13:34 +0300

    adds a shipping form for placing orders

--- src/app/components/check-out/check-out.component.ts
+++ src/app/components/check-out/check-out.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent implements OnInit {
+export class CheckOutComponent implements OnInit, OnDestroy {
+  shipping = {};
+  cart;
+  subscription: Subscription;
+  constructor(private cartS: ShoppingCartService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    const cart$ = await this.cartS.getCart();
+    this.subscription = cart$.subscribe(cart => { this.cart = cart; console.log(this.cart) });
   }
 
+  placeOrder(value) {
+    const order = {
+      datePlaced: new Date().getTime(),
+      shipping: value,
+      items: this.cart.items.map(i => {
+        return {
+          product: {
+            title: i.product.title,
+            imageUrl: i.product.imageUrl,
+            price: i.product.price
+          },
+          quantity: i.quantity,
+          // totalPrice: i.quantity * i.product.price
+          totalPrice: i.totalPrice
+        };
+      })
+    };
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
