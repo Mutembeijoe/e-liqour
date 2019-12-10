a53998efcb6e89e3bcc4c55a1e855abcdede4e68
mutembeijoe <mutembeijoe@gmail.com>
2019-12-10 09:09:10 +0300

    adds logic to reset cart after placing an order

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,15 +1,18 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private cartS: ShoppingCartService) { }
 
 
-  saveOrder(order) {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order) {
+    const result = await this.db.list('/orders').push(order);
+    this.cartS.clearCart();
+    return result;
   }
 }
