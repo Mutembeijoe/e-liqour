3f48cdd61215f40f87ce7f512ac08f115a2ca580
mutembeijoe <mutembeijoe@gmail.com>
2019-12-10 10:11:57 +0300

    adds logic to get all orders from database and display them in the admin orders component

--- src/app/components/admin/admin-orders/admin-orders.component.ts
+++ src/app/components/admin/admin-orders/admin-orders.component.ts
@@ -1,3 +1,4 @@
+import { OrderService } from './../../../services/order.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-orders.component.css']
 })
 export class AdminOrdersComponent implements OnInit {
-
-  constructor() { }
+  orders$;
+  constructor(private orderS: OrderService) {
+    this.orders$ = this.orderS.getOrders();
+  }
 
   ngOnInit() {
   }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,7 @@
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 import { ShoppingCartService } from './shopping-cart.service';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,17 @@ export class OrderService {
     const result = await this.db.list('/orders').push(order);
     this.cartS.clearCart();
     return result;
+  }
+
+  getOrders() {
+    // return this.db.list('/orders').valueChanges();
+    return this.db.list('/orders').snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    );
+  }
+  getProduct(key) {
+    return this.db.object(`/orders/${key}`).valueChanges();
   }
 }
